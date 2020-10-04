91858e2da03e650a00364b916296e2cfbdbad6b6
poplir <rpopli5@gmail.com>
2020-10-04 18:01:14 +0530

    Add files via upload

---
+++ share-blog-post-with-social-network.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Plugin Name: Share Blog Post With Social Network
+* Description: Using this plugin, visitors can share the URL of your blog post with their Facebook friends.
+* Version: 1.0
+* Author: Rajat Popli
+* Author URI: https://rajatpopli.online
+* Text domain: SPSN
+*/
+
+// If this file is called directly, abort
+if(!defined('ABSPATH'))  {
+	exit;
+}
+
+/********************************
+* Includes
+********************************/
+include ('includes/admin-page.php');
+include ('includes/display-functions.php');
